8931e7cbb2e84c49a44e2af1a5e97a5549a42a31
yousafshah <yousaf.shah@gmail.com>
2011-10-26 10:48:36 -0400

    updated test case

--- tests/generate_test_sequences.c
+++ tests/generate_test_sequences.c
@@ -1,24 +1,34 @@
 #include <stdlib.h>
 #include <time.h>
-
+#include <string.h>
+#include <math.h>
 #include "../../mersenne_twister/dSFMT.h"
 
+
+#define MODEL_TYPE_NORMAL 0
+#define MODEL_TYPE_REVERSE 1
+#define MODEL_TYPE_FORWARD 2
+#define MODEL_TYPE_PALINDROMIC 3
 
 int main(int number_arguments, char **arguments) {
     int character, i, j, k;
     char **sequences, **motifs;
     int **start_position;
-    int number_sequences, sequence_length;
-    int number_motifs, motif_length;
+    int number_sequences, sequence_length,palindrome_length;
+    /* motif_build 1 = exact motif; 2=probabilistic random motif
+    */
+    int number_motifs, motif_length,motif_build,motif_model;
     int seed;
 
     printf("arguments[1]: %s\n", arguments[1]);
 
     number_sequences = -1;
     sequence_length = -1;
-    number_motifs = -1;
+    number_motifs =-1;
     motif_length = -1;
-
+    motif_model = 1;
+    motif_build = 1;
+    palindrome_length=8; //length of palindromic part of motif starting at 0
     seed = time(NULL);
 
     for (i = 1; i < number_arguments; i++) {
@@ -34,12 +44,20 @@ int main(int number_arguments, char **arguments) {
         } else if ( !strcmp(arguments[i], "--motif_length")) {
             motif_length = atoi(arguments[++i]);
 
+        } else if ( !strcmp(arguments[i], "--motif_model")) {
+        	motif_model = atoi(arguments[++i]);
+
+        } else if ( !strcmp(arguments[i], "--motif_build")) {
+            motif_build = atoi(arguments[++i]);
+
+ 	} else if ( !strcmp(arguments[i], "--palindrome_length")) {
+            palindrome_length = atoi(arguments[++i]);
+
         } else if ( !strcmp(arguments[i], "--seed")) {
             seed = atoi(arguments[++i]);
 
         }
     }
-
 
     sequences = (char**)malloc(number_sequences * sizeof(char*));
     for (i = 0; i < number_sequences; i++) sequences[i] = (char*)malloc((sequence_length + 1) * sizeof(char));
@@ -72,18 +90,66 @@ int main(int number_arguments, char **arguments) {
 //        printf("%s\n\n", sequences[i]);
     }
 
+    if((MODEL_TYPE_REVERSE == motif_model||MODEL_TYPE_NORMAL == motif_model) && 1==motif_build)
+    {
     for (i = 0; i < number_motifs; i++) {
         for (j = 0; j < motif_length; j++) {
             character = (int)(dsfmt_gv_genrand_open_close() * 4.0);
 
             if (character == 0) motifs[i][j] = 'A';
             if (character == 1) motifs[i][j] = 'C';
-            if (character == 2) motifs[i][j] = 'G';
-            if (character == 3) motifs[i][j] = 'T';
+            if (character == 2)	motifs[i][j] = 'G';
+            if (character == 3)	motifs[i][j] = 'T';
         }
         motifs[i][motif_length] = '\0';
         printf("motif [%d]: [%s]\n", i, motifs[i]);
     }
+    }
+   
+    //Palindromic motif
+    if(MODEL_TYPE_PALINDROMIC == motif_model && 1 == motif_build)
+    {
+	if( 2*palindrome_length > motif_length)
+             {
+                printf("Palindrome lenght should be atmost half of the motif_length\n Palindrome Length=%d Motif Length=%d\n",palindrome_length, motif_length);
+                return;
+             }
+    for (i = 0; i < number_motifs; i++) {
+        for (j = 0; j < motif_length-palindrome_length; j++) {
+            character = (int)(dsfmt_gv_genrand_open_close() * 4.0);
+
+            if (character == 0)
+            	{
+            	motifs[i][j] = 'A';
+		if(j<palindrome_length)
+            	motifs[i][motif_length-j-1] = 'T';
+            	}
+            if (character == 1)
+		{
+            	motifs[i][j] = 'C';
+		if(j<palindrome_length)
+               	motifs[i][motif_length-j-1] = 'G';
+		}
+            if (character == 2)
+		{
+            	motifs[i][j] = 'G';
+		if(j<palindrome_length)
+                motifs[i][motif_length-j-1] = 'C';
+            	}
+
+            if (character == 3)
+		{
+            	motifs[i][j] = 'T';
+		if(j<palindrome_length)
+                motifs[i][motif_length-j-1] = 'A';
+		}
+
+        }
+        motifs[i][motif_length] = '\0';
+        printf("motif [%d]: [%s]\n", i, motifs[i]);
+    }
+	}
+
 //    printf("overrepresented sequence: [%s]\n", motifs);
 
     for (i = 0; i < number_sequences; i++) {
@@ -91,16 +157,38 @@ int main(int number_arguments, char **arguments) {
             start_position[i][j] = (int)(dsfmt_gv_genrand_open_close() * ((sequence_length - motif_length) - (j* motif_length)));
 //        printf("positions in sequence [%d]: %d to %d\n", i, start_position[i], start_position[i] + motif_length);
 
-            if (j > 0 && start_position[i][j-1]) {
-                if ((start_position[i][j] >= start_position[i][j-1]) && (start_position[i][j] < start_position[i][j-1] + motif_length)) {
+            if (j > 0 /*&& start_position[i][j-1]*/) {
+                if ((start_position[i][j] >= start_position[i][j-1]) && (start_position[i][j] < (start_position[i][j-1] + motif_length))) {
                     printf("[%d] start position %d collided with start position %d\n", i, start_position[i][j], start_position[i][j-1]);
                     start_position[i][j] += motif_length;
                 }
+                if((start_position[i][j] <= start_position[i][j-1]) && ((start_position[i][j]+ motif_length)> start_position[i][j-1] ))
+                {
+                	printf("[%d] start position %d collided with start position %d\n", i, start_position[i][j], start_position[i][j-1]);
+                	if(start_position[i][j]-motif_length >= 0)
+                	start_position[i][j] -= motif_length;
+                	else
+                	start_position[i][j] += 2*motif_length;
+                }
             }
+	   if(MODEL_TYPE_REVERSE == motif_model)
+	   {
+	     for (k = 0; k < motif_length; k++)
+               {
+	       if(motifs[j][motif_length-k-1] == 'A')
+		 sequences[i][start_position[i][j] + k] = 'T';
+               if(motifs[j][motif_length-k-1]=='T')
+                 sequences[i][start_position[i][j] + k] = 'A';
+               if(motifs[j][motif_length-k-1]=='G')
+                 sequences[i][start_position[i][j] + k] = 'C';
+               if(motifs[j][motif_length-k-1]=='C')
+                 sequences[i][start_position[i][j] + k] = 'G';
+	       }
+	   }
+	   else
+            for (k = 0; k < motif_length; k++)
+            	sequences[i][start_position[i][j] + k] = motifs[j][k];
 
-            for (k = 0; k < motif_length; k++) {
-                sequences[i][start_position[i][j] + k] = motifs[j][k];
-            }
         }
     }
 
@@ -114,4 +202,5 @@ int main(int number_arguments, char **arguments) {
 
         printf("%s\n\n", sequences[i]);
     }
+
 }
